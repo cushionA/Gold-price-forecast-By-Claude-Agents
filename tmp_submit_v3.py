8d9aed3036f85f9d9968497a14372d1e78e40b5d
cushionA <tatuki.m1105@gmail.com>
2026-02-14 19:49:20 +0900

    kaggle: dxy attempt 1 - version 3 submitted (yfinance fix)

---
+++ tmp_submit_v3.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+import sys
+
+os.environ['KAGGLE_API_TOKEN'] = 'KGAT_357794ad87b13a4ecd000b7ff9ac57ea'
+os.environ['PYTHONUTF8'] = '1'
+
+result = subprocess.run(
+    ['kaggle', 'kernels', 'push', '-p', 'notebooks/dxy_1'],
+    capture_output=True,
+    text=True,
+    encoding='utf-8',
+    errors='replace'
+)
+
+print(result.stdout)
+print(result.stderr, file=sys.stderr)
+sys.exit(result.returncode)
